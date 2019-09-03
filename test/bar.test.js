7c8d1c305561c0e655c2b16a89445aa32e1ce398
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-09-03 15:48:11 -0700

    can get all the bar reviews and passed test

--- test/bar.test.js
+++ test/bar.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Bar = require('../lib/models/Bar');
 
 describe('bar routes', () => {
   beforeAll(() => {
@@ -33,6 +34,35 @@ describe('bar routes', () => {
           address: '725 SW 10th Ave, Portland, OR 97205',
           notes: 'alchemy folks fav place',
           __v: expect.any(Number)
+        });
+      });
+  });
+
+  it('can get all the bars', async() => {
+    const bar = await Bar.create([
+      {
+        name: 'Bettys',
+        address: '435 W Nationwide Blvd, Columbus, OH 43215',
+        notes: '$3 Betty Ford Clinic Special drink'
+      },
+      {
+        name: 'Aalto Lounge',
+        address: '3356 SE Belmont St, Portland, OR 97214',
+        notes: '$3 drinks from 5-7pm'
+      }
+    ]);
+
+    return request(app)
+      .get('/api/v1/bars')
+      .then(res => {
+        const barsJSON = JSON.parse(JSON.stringify(bar));
+        barsJSON.forEach(bar => {
+          expect(res.body).toContainEqual({
+            name: bar.name,
+            address: bar.address,
+            notes: bar.notes,
+            _id: bar._id
+          });
         });
       });
   });
